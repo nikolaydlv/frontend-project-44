25f327c979ba369e2936875f945ed37577e12d82
Николай <Nikolay@MacBook-Pro-Nikolaj.local>
2023-01-08 17:27:54 +0300

    add eslint and fix errors

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 function askName() {
-const userName = readlineSync.question('May I have your name?');
-console.log('Hello, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
 }
-export { askName };
+export default askName;
