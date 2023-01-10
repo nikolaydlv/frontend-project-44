17b3f4d4e4dc490a9afed1b554134ae84cbb1ab6
Николай <Nikolay@MacBook-Pro-Nikolaj.local>
2023-01-10 19:06:12 +0300

    add brain-gcd

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,31 @@
+import gamesLogic from '../index.js';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const gameStart = () => {
+  const task = 'Find the greatest common divisor of given numbers.';
+
+  const gcd = (num1, num2) => {
+    let firstNumber = num1;
+    let secondNumber = num2;
+    while (firstNumber !== secondNumber) {
+      if (firstNumber > secondNumber) {
+        firstNumber -= secondNumber;
+      } else {
+        secondNumber -= firstNumber;
+      }
+    }
+    return firstNumber.toString();
+  };
+
+  const gameConditions = () => {
+    const num1 = getRandomNumber(1, 30);
+    const num2 = getRandomNumber(1, 30);
+    const question = `${num1} ${num2}`;
+    const correctAnswer = gcd(num1, num2);
+    return [question, correctAnswer];
+  };
+  return gamesLogic(task, gameConditions);
+};
+
+export default gameStart;
