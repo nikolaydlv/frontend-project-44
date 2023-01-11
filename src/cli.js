42b18f17a2af3f6375bf7d6c0b1b2e6a6378ab5b
Николай <Nikolay@MacBook-Pro-Nikolaj.local>
2023-01-11 14:23:56 +0300

    remove brain-games.js and cli.js

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-function askName() {
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`);
-}
-export default askName;
